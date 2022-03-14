1c855634cc897bea140cb7b67f951f15e901db06
BidanGitau <bydangitau@gmail.com>
2022-03-14 05:26:42 +0300

    js toggle

--- js/dom.js
+++ js/dom.js
@@ -1,12 +1,12 @@
-// $(document).ready(function() {
-//     $('.service-icon').click(function() {
-//         $(".description").toggle();
-//     });
-// })
-let moringaStudent = {
-    firstName: "morris",
-    LastName: "kwambulu",
-    Level: 1,
-    track: ["Prep", "JavaScript", "Python", "Django"],
-    enrollmentStatus: true
-};
+$(document).ready(function() {
+        $('.service-icon').click(function() {
+            $(".description").toggle();
+        });
+    })
+    // Let moringaStudent = {
+    //     firstName: ”Morris”,
+    //     LastName: ”Kwambulu”,
+    //     Level: 1,
+    //     track: ["Prep", "JavaScript", "Python", "Django"],
+    //     enrollmentStatus: true
+    // };
