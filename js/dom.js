636119478f83bc5147b5a2297c4c8087b2c8d8f0
BidanGitau <bydangitau@gmail.com>
2022-03-14 05:04:45 +0300

    contact section

--- js/dom.js
+++ js/dom.js
@@ -1,5 +1,12 @@
-$(document).ready(function() {
-    $('.service-icon').click(function() {
-        $(".description").toggle();
-    });
-})
+// $(document).ready(function() {
+//     $('.service-icon').click(function() {
+//         $(".description").toggle();
+//     });
+// })
+let moringaStudent = {
+    firstName: "morris",
+    LastName: "kwambulu",
+    Level: 1,
+    track: ["Prep", "JavaScript", "Python", "Django"],
+    enrollmentStatus: true
+};
